1b78187341a92f60478ea9aa5039ccfe9c413046
Nijat Muhtar <nijatmn@gmail.com>
2022-06-10 15:17:11 -0400

    finished setting up starter project

---
+++ src/test/java/RegressionTest.java
@@ -0,0 +1,9 @@
+import org.testng.annotations.Test;
+
+public class RegressionTest {
+
+    @Test
+    public void testCase20() {
+
+    }
+}
---
+++ src/test/java/SmokeTest.java
@@ -0,0 +1,9 @@
+import org.testng.annotations.Test;
+
+public class SmokeTest {
+
+    @Test
+    public void testCase1() {
+
+    }
+}
